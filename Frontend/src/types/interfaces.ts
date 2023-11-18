4e19882b1d6c854284f8d51bfa75e1f2e0c5ff46
Ivan Garcia <ivanggv2@gmail.com>
2023-11-18 15:59:06 +0100

    created hook useUser tomanage the user

--- Frontend/src/types/interfaces.ts
+++ Frontend/src/types/interfaces.ts
@@ -4,8 +4,18 @@ export interface UiData {
   isLoading: boolean;
 }
 
+export interface LoginUser {
+  userName: string;
+  password: string;
+}
+
 export interface UserLoged {
   userName: string;
+  email: string;
+  weight: string;
+  height: string;
+  birthDate: string;
+  registerDate: string;
   token: string;
   isLogged: boolean;
 }
