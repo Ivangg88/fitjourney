92469f95a331e361aab4b7f3bd090c662da1671d
Ivan Garcia <ivanggv2@gmail.com>
2023-11-18 16:50:54 +0100

    modified the naming on the test and loaded the Wrapper

--- Frontend/src/redux/slices/userSlice/userSlice.test.ts
+++ Frontend/src/redux/slices/userSlice/userSlice.test.ts
@@ -5,15 +5,17 @@ import {
   logoutUserActionCreator,
   userReducer,
 } from "./userSlice";
+import { initialUser } from "../../../utils/initialStates";
 
 describe("Given a loginUserActionCreator", () => {
+  const userToLogin: UserLoged = {
+    ...initialUser,
+    isLogged: true,
+    token: "",
+    userName: "",
+  };
   describe("When is called", () => {
     test("Then it should return an action with the type user/loginUser", () => {
-      const userToLogin: UserLoged = {
-        isLogged: true,
-        token: "",
-        userName: "",
-      };
       const actionType = "user/loginUser";
       const expectedAction = {
         type: actionType,
@@ -47,14 +49,15 @@ describe("Given an userReducer function", () => {
   describe("When is called with an action type 'user/loginUser' and a payload with the user to login", () => {
     test("Then it should set the property isLogged of the state at true", () => {
       const userToLogin: UserLoged = {
+        ...initialUser,
         isLogged: true,
         token: "",
         userName: "",
       };
-      const initialUser = getInitialUser();
+      const receivedInitialUser = getInitialUser();
 
       const action = loginUserActionCreator(userToLogin);
-      const userLogged = userReducer(initialUser, action);
+      const userLogged = userReducer(receivedInitialUser, action);
 
       expect(userLogged.isLogged).toBe(true);
     });
@@ -62,14 +65,15 @@ describe("Given an userReducer function", () => {
 
   describe("When is called with an action type 'user/logoutUser'", () => {
     test("Then it should set the property isLogged of the state at false", () => {
-      const initialUser: UserLoged = {
+      const userToLogin: UserLoged = {
+        ...initialUser,
         isLogged: true,
         token: "",
         userName: "",
       };
 
       const action = logoutUserActionCreator();
-      const userLogged = userReducer(initialUser, action);
+      const userLogged = userReducer(userToLogin, action);
 
       expect(userLogged.isLogged).toBe(false);
     });
